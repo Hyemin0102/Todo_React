3c546fac5040556891dd1ef4494af7c5c2a5b129
HyeMin <128768462+Hyemin0102@users.noreply.github.com>
2023-08-03 13:12:26 +0900

    삭제,체크 기능 추가

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import TodoTemplate from './components/TodoTemplate';
 
 function App() {
   const [todos, setTodos] = useState([]);
-  console.log(todos)
+  //console.log(todos)
 
   //할일 작성할때마다 id값 1씩 추가되도록, 1부터 설정
   const nextId = useRef(1);
@@ -24,11 +24,25 @@ function App() {
     }
   ,[todos]);
 
+  //todos 배열 중 id값이 파라미터로 전달 받은 id와 같지 않은 것만 새 배열로 만듦
+  const onRemove=useCallback((id)=>{
+    setTodos(todos.filter(todo=>todo.id !== id));
+  },[todos])
+
+  //클릭한 id todos배열 중 checked상태를 반전시킴
+  const onToggle = useCallback((id)=>{
+    setTodos(
+      todos.map(todo => (
+        todo.id === id ? {...todo, checked: !todo.checked} : todo
+      ))
+    )
+  },[todos])
+
   return (
     <div>
       <TodoTemplate>
         <TodoInsert onInsert={onInsert}/>
-        <TodoList todos={todos}/>
+        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
       </TodoTemplate>
     </div>
   );
