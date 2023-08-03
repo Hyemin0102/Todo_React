4522cff0071539f461a09e2a41489a7d335f1272
Hyemin0102 <128768462+Hyemin0102@users.noreply.github.com>
2023-08-03 17:58:41 +0900

    성능 최적화

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 
-import { useCallback, useRef, useState } from 'react';
+import { useCallback, useReducer, useRef } from 'react';
 import './App.css';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
+//데이터 많이 출력하기 위해 생성 
+function createBulkTodos(){
+  const array = [];
+  for(let i = 1; i<=2500; i++){
+    array.push({
+      id:i,
+      text:`할일${i}`,
+      checked: false
+    });
+  }
+  return array;
+};
+
+function todoReducer(todos, action){
+  switch(action.type){
+    case 'INSERT' :
+      return todos.concat(action.todo);
+    case 'REMOVE' :
+      return todos.filter(todo=>todo.id !== action.id);
+    case 'TOGGLE' :
+      return  todos.map(todo => (
+        todo.id === action.id ? {...todo, checked: !todo.checked} : todo
+      ));
+    default:
+      return todos;
+  }
+}
+
+
 function App() {
-  const [todos, setTodos] = useState([]);
-  //console.log(todos)
+  const [todos, dispatch] = useReducer(todoReducer,undefined,createBulkTodos);
+  //useReducer(함수, 초기상태)
+  
 
   //할일 작성할때마다 id값 1씩 추가되도록, 1부터 설정
-  const nextId = useRef(1);
+  const nextId = useRef(2501);
 
   const onInsert = useCallback(
     value=>{
@@ -19,24 +49,20 @@ function App() {
         text: value,
         checked:false
       };
-      setTodos(todos.concat(todo));
+      dispatch({type:'INSERT', todo});
       nextId.current += 1; //nextId에 1씩 더하기
     }
-  ,[todos]);
+  ,[]);
 
   //todos 배열 중 id값이 파라미터로 전달 받은 id와 같지 않은 것만 새 배열로 만듦
   const onRemove=useCallback((id)=>{
-    setTodos(todos.filter(todo=>todo.id !== id));
-  },[todos])
+    dispatch({type:'REMOVE', id});
+  },[])
 
   //클릭한 id todos배열 중 checked상태를 반전시킴
   const onToggle = useCallback((id)=>{
-    setTodos(
-      todos.map(todo => (
-        todo.id === id ? {...todo, checked: !todo.checked} : todo
-      ))
-    )
-  },[todos])
+    dispatch({type:'TOGGLE', id})
+  },[]);
 
   return (
     <div>
@@ -49,3 +75,30 @@ function App() {
 }
 
 export default App;
+
+
+/* const onInsert = useCallback(
+  value=>{
+    const todo = {
+      id: nextId.current,
+      text: value,
+      checked:false
+    };
+    setTodos(todos => todos.concat(todo));
+    nextId.current += 1; //nextId에 1씩 더하기
+  }
+,[]);
+
+//todos 배열 중 id값이 파라미터로 전달 받은 id와 같지 않은 것만 새 배열로 만듦
+const onRemove=useCallback((id)=>{
+  setTodos(todos => todos.filter(todo=>todo.id !== id));
+},[])
+
+//클릭한 id todos배열 중 checked상태를 반전시킴
+const onToggle = useCallback((id)=>{
+  setTodos(todos =>
+    todos.map(todo => (
+      todo.id === id ? {...todo, checked: !todo.checked} : todo
+    ))
+  )
+},[]) */
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -4,23 +4,27 @@ import { MdCheckBoxOutlineBlank,
   MdCheckBox,
   MdIndeterminateCheckBox
 } from "react-icons/md";
+import React from 'react';
 
-const TodoListItem = ({todo, onRemove,onToggle}) => {
+const TodoListItem = ({todo, onRemove,onToggle,style}) => {
   const {id, text, checked} = todo;
 
   //classnames 라이브러리 사용, 첫번째 인자는 공통 클라스, 두번째 인자는 조건부 설정
   //todo에서 받아온 checked의 상태에 따라 클라스 적용되고 따라서 클라스 이름과 props의 이름 같아야함
   return(
-    <div className='TodoListItem'>
-      <div className={classNames('checkbox',{checked})} onClick={()=>onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className='text'>{text}</div>
-      </div>
-      <div className='remove' onClick={()=>onRemove(id)}>
-        <MdIndeterminateCheckBox />
+    <div className='TodoListItem-virtualized' style={style}>
+      <div className='TodoListItem'>
+        <div className={classNames('checkbox',{checked})} onClick={()=>onToggle(id)}>
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className='text'>{text}</div>
+        </div>
+        <div className='remove' onClick={()=>onRemove(id)}>
+          <MdIndeterminateCheckBox />
+        </div>
       </div>
     </div>
+    
   )
 };
 
-export default TodoListItem;
+export default React.memo(TodoListItem);
